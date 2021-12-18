2789689325fbe7c928fe7f3f83babfb45c53eff2
Santiago Passafiume <santiagopassafiume@protonmail.com>
2021-12-17 20:53:53 -0900

    improve paths and add hardcoded examples to test them

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import enum
+from os import stat
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.param_functions import Body
 from pydantic import BaseModel
+from random import randrange
+
 
 app = FastAPI()
 
@@ -14,6 +18,31 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [
+    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "favorite foods", "content": "I like pizza", "id": 2},
+]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
+"""
+Be careful when you create new paths, and check the order in which they're created, for example:
+
+/posts/{id} being created before /posts/latests means that when we want to call "/posts/latest", "latests" will be used as a parameter for the first path, not the second, which leads to us never being able to call "/posts/latest".
+"""
+
+
 @app.get("/")
 def root():
     return {"message": "Santiago's API"}
@@ -21,9 +50,45 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "These are your posts!"}
+    return {"data": my_posts}
 
 
-@app.post("/posts")
-def create_posts(post: Post):
-    return {"data": post}
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+# {id} is a path parameter
+@app.get("/posts/{id}")
+def get_post(
+    id: int,
+):  # "id: int" asks FastAPI to make sure the value passed is an integer.
+    post = find_post(int(id))
+    if not post:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, {"message": f"post with id: {id} was not found"}
+        )
+        # Keeping the commented code to see how not to do it:
+        #
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id: {id} was not found"}
+    return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # deleting post
+    # find the index in the array that has required ID
+    # my_posts.pop(index)
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {id} does not exist.",
+        )
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
