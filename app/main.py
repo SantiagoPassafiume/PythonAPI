3b6954902f3cc536bd280fadfe4adecf766a6aee
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-24 07:53:30 -0300

    cleanup return statement of each route

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def root():
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
 
-    return {"data": posts}
+    return posts
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
@@ -41,7 +41,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post
 
 
 @app.get("/posts/{id}")
@@ -51,7 +51,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     if not post:
         raise_404_not_found(id)
 
-    return {"post_detail": post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -79,4 +79,4 @@ def update_post(
 
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post_query.first()}
+    return post_query.first()
