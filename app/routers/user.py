1ae976fd1337dc0ea814ebd0ba6731f8962dee49
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-24 10:41:08 -0300

    create router inside 'app/routers/user.py'

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from ..database import engine, get_db
@@ -10,8 +10,12 @@ Be careful when you create new paths, and check the order in which they're creat
 /posts/{id} being created before /posts/latests means that when we want to call "/posts/latest", "latest" will be used as a parameter for the first path, not the second, which leads to us never being able to call "/posts/latest".
 """
 
+router = APIRouter()
 
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+
+@router.post(
+    "/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
+)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     # Hash the password
@@ -26,7 +30,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@app.get("/users/{id}", response_model=schemas.UserOut)
+@router.get("/users/{id}", response_model=schemas.UserOut)
 def create_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
 
