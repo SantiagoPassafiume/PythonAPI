8087d4bb858d843b1921ef7532d6d640ee528cda
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-24 10:00:22 -0300

    clean imports inside 'models.py'

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
-from enum import unique
 from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean
-from sqlalchemy.sql.expression import null, text
+from sqlalchemy.sql.expression import text
 from .database import Base
 
 
