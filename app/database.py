f572305ddb90a375d769f870dcf6b3297480deeb
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-23 21:37:49 -0300

    create base code for ORM inside a file called database.py

---
+++ app/database.py
@@ -0,0 +1,13 @@
+import imp
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Format of the database url: "postgresql://<username>:<password>@<ip-address/hostname>/<database_name>"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:santiago@localhost/fastapi"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
