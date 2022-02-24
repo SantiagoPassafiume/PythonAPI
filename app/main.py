bfc9b4fbb2009d0badedef5f1617bbc6cd1ec4da
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-24 10:45:23 -0300

    link 'post' and 'user' routers to main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-
+from .routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(post.router)
+app.include_router(user.router)
 
 
 @app.get("/")
