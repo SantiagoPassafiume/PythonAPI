35ea0edebfcac9d74273522c18745c7fef81db87
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-23 21:46:11 -0300

    create db dependency inside main.py

--- app/main.py
+++ app/main.py
@@ -4,11 +4,19 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from . import models
-from .database import engine
+from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel):
