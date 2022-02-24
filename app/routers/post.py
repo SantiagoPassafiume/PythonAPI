a8ffbd028f7fe22f0a669464e535f32a84135a23
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-24 10:40:48 -0300

    create router inside 'app/routers/post.py'

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,8 +1,10 @@
 from typing import List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from ..database import engine, get_db
+
+router = APIRouter()
 
 
 def raise_404_not_found(id):
@@ -19,14 +21,14 @@ Be careful when you create new paths, and check the order in which they're creat
 """
 
 
-@app.get("/posts", response_model=List[schemas.Post])
+@router.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
 
     return posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
@@ -36,7 +38,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 
-@app.get("/posts/{id}", response_model=schemas.Post)
+@router.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
@@ -46,7 +48,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return post
 
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id)
 
@@ -59,7 +61,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}", response_model=schemas.Post)
+@router.put("/posts/{id}", response_model=schemas.Post)
 def update_post(
     id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)
 ):
